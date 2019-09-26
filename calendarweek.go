6720e153b6f983e9efab470e2b6c68c58d6a2d8a
Lukas Wöhrl <lukas@woehrl.net>
2019-09-26 08:11:40 +0200

    fixed hidpi on windows and addes tests for the cw iterator

--- calendarweek.go
+++ calendarweek.go
@@ -4,25 +4,30 @@ import (
 	"time"
 )
 
-type CalendarWeekIterator struct{
+type CalendarWeekIterator struct {
 	ChangedCh chan int
 }
 
 func currentCalendarWeekIterator() CalendarWeekIterator {
-	const duration = 10 * time.Minute;
-	
+	return calendarWeekIteratorWithCustomProvider(10*time.Minute, func() int {
+		_, currentWeekNumber := time.Now().UTC().ISOWeek()
+		return currentWeekNumber
+	})
+}
+
+func calendarWeekIteratorWithCustomProvider(duration time.Duration, currentWeekProvider func() int) CalendarWeekIterator {
 	ch := make(chan int)
 
 	go func() {
-		var lastWeekNumber = -1;
-		for ticker := time.NewTicker(duration) ; ; <-ticker.C {
-			_, currentWeekNumber := time.Now().UTC().ISOWeek()
-			if(lastWeekNumber != currentWeekNumber){
+		var lastWeekNumber = -1
+		for ticker := time.NewTicker(duration); ; <-ticker.C {
+			currentWeekNumber := currentWeekProvider()
+			if lastWeekNumber != currentWeekNumber {
 				lastWeekNumber = currentWeekNumber
 				ch <- currentWeekNumber
 			}
 		}
 	}()
 
-	return CalendarWeekIterator{ch};
+	return CalendarWeekIterator{ch}
 }
---
+++ dpiaware_windows.go
@@ -0,0 +1,16 @@
+// +build windows
+
+package main
+
+import (
+	"golang.org/x/sys/windows"
+)
+
+var (
+	u32                 = windows.NewLazySystemDLL("User32.dll")
+	pSetProcessDPIAware = u32.NewProc("SetProcessDPIAware")
+)
+
+func EnableDpiAwareness() {
+	pSetProcessDPIAware.Call()
+}
