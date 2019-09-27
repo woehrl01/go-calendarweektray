41eb73619aaa63d55772323bc4b1b91f4bbc43d4
Lukas Wöhrl <luwoehrl@microsoft.com>
2019-09-27 09:40:15 +0200

    add upcoming calendarweek start dates to the context menu

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/getlantern/systray"
+	"github.com/goodsign/monday"
+	"github.com/snabb/isoweek"
 )
 
 func main() {
@@ -11,11 +15,23 @@ func main() {
 	systray.Run(onReady, func() {})
 }
 
+var menus []*systray.MenuItem
+
 func onReady() {
 	systray.SetTitle("Kalenderwoche")
 
+	addMenuItemsForUpcomingCalendarWeekDates()
+
 	go keepWeekNumberIconUpToDate()
 	go quitOnMenu()
+}
+
+func addMenuItemsForUpcomingCalendarWeekDates() {
+	for i := 0; i < 15; i++ {
+		menus = append(menus, systray.AddMenuItem("refresh", ""))
+	}
+
+	systray.AddSeparator()
 }
 
 func keepWeekNumberIconUpToDate() {
@@ -34,4 +50,23 @@ func quitOnMenu() {
 func updateIconAndTooltip(weekNumber int) {
 	systray.SetIcon(generateImage(weekNumber))
 	systray.SetTooltip(fmt.Sprintf("Aktuelle Kalenderwoche: %d", weekNumber))
+
+	refreshUpcomingCalendarWeekItems()
+}
+
+func refreshUpcomingCalendarWeekItems() {
+	year, week := time.Now().ISOWeek()
+
+	for index, _ := range menus {
+		week++
+		startDate := isoweek.StartTime(year, week, time.Local)
+
+		if !isoweek.Validate(year, week) {
+			week = 1
+			year++
+		}
+
+		text := fmt.Sprintf("KW %d: %s", week, monday.Format(startDate, "02. January 2006", monday.LocaleDeDE))
+		menus[index].SetTitle(text)
+	}
 }
