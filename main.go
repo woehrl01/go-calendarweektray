5120da536c776bf21ce32d2da76c9bb21da9c501
Lukas Wöhrl <lukas@woehrl.net>
2019-10-13 21:33:10 +0200

    Improve testability and test coverage

--- main.go
+++ main.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/getlantern/systray"
 	"github.com/goodsign/monday"
-
-	ole "github.com/go-ole/go-ole"
-	"github.com/go-ole/go-ole/oleutil"
 )
 
 func main() {
@@ -28,29 +24,6 @@ func onReady() {
 	go quitOnMenu()
 }
 
-func GoToDate(dateToGo time.Time) {
-	date := dateToGo.Format("02/01/2006")
-	ole.CoInitialize(0)
-	defer ole.CoUninitialize()
-	unknown, _ := oleutil.CreateObject("Outlook.Application")
-	defer unknown.Release()
-	outlook, _ := unknown.QueryInterface(ole.IID_IDispatch)
-	defer outlook.Release()
-
-	ns := oleutil.MustCallMethod(outlook, "GetNamespace", "MAPI").ToIDispatch()
-	calendar := oleutil.MustCallMethod(ns, "GetDefaultFolder", 9).ToIDispatch()
-	explorer := oleutil.MustCallMethod(outlook, "ActiveExplorer").ToIDispatch()
-	if explorer == nil {
-		oleutil.MustCallMethod(calendar, "Display")
-		explorer = oleutil.MustCallMethod(outlook, "ActiveExplorer").ToIDispatch()
-	}
-
-	oleutil.MustCallMethod(explorer, "CurrentFolder", calendar)
-	view := oleutil.MustGetProperty(explorer, "CurrentView").ToIDispatch()
-	oleutil.MustCallMethod(view, "GoToDate", date).ToIDispatch()
-	oleutil.MustCallMethod(explorer, "Activate")
-}
-
 func addMenuItemsForUpcomingCalendarWeekDates() {
 	for i := 0; i < 15; i++ {
 		var index = i
@@ -59,7 +32,7 @@ func addMenuItemsForUpcomingCalendarWeekDates() {
 			for {
 				<-menus[index].ClickedCh
 				_, dateToGo := offsetCalendarWeekToDate(index)
-				GoToDate(dateToGo)
+				goToDate(dateToGo)
 			}
 		}()
 	}
