375d7181cb82c6b875624071b998ab1d271901e7
Lukas Wöhrl <luwoehrl@microsoft.com>
2019-10-13 21:00:56 +0200

    Add feature to jump to specific outlook calendar week

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"github.com/getlantern/systray"
 	"github.com/goodsign/monday"
 	"github.com/snabb/isoweek"
+
+	ole "github.com/go-ole/go-ole"
+	"github.com/go-ole/go-ole/oleutil"
 )
 
 func main() {
@@ -26,9 +29,39 @@ func onReady() {
 	go quitOnMenu()
 }
 
+func GoToDate(date string) {
+	ole.CoInitialize(0)
+	defer ole.CoUninitialize()
+	unknown, _ := oleutil.CreateObject("Outlook.Application")
+	defer unknown.Release()
+	outlook, _ := unknown.QueryInterface(ole.IID_IDispatch)
+	defer outlook.Release()
+
+	ns := oleutil.MustCallMethod(outlook, "GetNamespace", "MAPI").ToIDispatch()
+	calendar := oleutil.MustCallMethod(ns, "GetDefaultFolder", 9).ToIDispatch()
+	explorer := oleutil.MustCallMethod(outlook, "ActiveExplorer").ToIDispatch()
+	if explorer == nil {
+		oleutil.MustCallMethod(calendar, "Display")
+		explorer = oleutil.MustCallMethod(outlook, "ActiveExplorer").ToIDispatch()
+	}
+
+	oleutil.MustCallMethod(explorer, "CurrentFolder", calendar)
+	view := oleutil.MustGetProperty(explorer, "CurrentView").ToIDispatch()
+	oleutil.MustCallMethod(view, "GoToDate", date).ToIDispatch()
+	oleutil.MustCallMethod(explorer, "Activate")
+}
+
 func addMenuItemsForUpcomingCalendarWeekDates() {
 	for i := 0; i < 15; i++ {
+		var index = i
 		menus = append(menus, systray.AddMenuItem("refresh", ""))
+		go func() {
+			for {
+				<-menus[index].ClickedCh
+				dateToGo := indexToDate(index).Format("02/01/2006")
+				GoToDate(dateToGo)
+			}
+		}()
 	}
 
 	systray.AddSeparator()
@@ -69,4 +102,22 @@ func refreshUpcomingCalendarWeekItems() {
 		text := fmt.Sprintf("KW %d: %s", week, monday.Format(startDate, "02. January 2006", monday.LocaleDeDE))
 		menus[index].SetTitle(text)
 	}
+}
+
+func indexToDate(ix int) time.Time {
+	year, week := time.Now().ISOWeek()
+	for index, _ := range menus {
+		week++
+		startDate := isoweek.StartTime(year, week, time.Local)
+
+		if !isoweek.Validate(year, week) {
+			week = 1
+			year++
+		}
+
+		if index == ix {
+			return startDate
+		}
+	}
+	return time.Now()
 }
