6aeef28f2a00a72df6ab5b1b985382c9b5cda3b9
Lukas Wöhrl <lukas@woehrl.net>
2019-12-09 20:09:02 +0100

    Reduced filesize from 8MB to about 2 MB

--- release.ps1
+++ release.ps1
@@ -2,4 +2,5 @@ $now = Get-Date -UFormat "%Y-%m-%d_%T"
 $sha1 = (git rev-parse HEAD).Trim().Substring(0, 8)
 $version = (git describe --tags $(git rev-list --tags --max-count=1)).Trim()
 
-go build -ldflags "-X main.sha1ver=$sha1 -X main.buildTime=$now  -X main.semVer=$version -H=windowsgui" -o Kalenderwoche.exe
+go build -ldflags "-s -w -X main.sha1ver=$sha1 -X main.buildTime=$now  -X main.semVer=$version -H=windowsgui" -o Kalenderwoche.exe
+upx .\Kalenderwoche.exe
