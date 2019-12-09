f6b72a123c93bfe63e1b7e31efe9973c80e16699
Lukas Wöhrl <lukas@woehrl.net>
2019-12-09 20:23:03 +0100

    reduced the size even more less than 2 MB

--- release.ps1
+++ release.ps1
@@ -3,4 +3,4 @@ $sha1 = (git rev-parse HEAD).Trim().Substring(0, 8)
 $version = (git describe --tags $(git rev-list --tags --max-count=1)).Trim()
 
 go build -ldflags "-s -w -X main.sha1ver=$sha1 -X main.buildTime=$now  -X main.semVer=$version -H=windowsgui" -o Kalenderwoche.exe
-upx .\Kalenderwoche.exe
+upx --brute .\Kalenderwoche.exe
